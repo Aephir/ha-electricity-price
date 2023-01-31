f3ce095c25bcd5986df028b11fc1e17cdda70d5e
Aephir <waldenb@gmail.com>
2023-01-31 20:08:51 +0100

    added update_listener and unload_entry

--- custom_components/electricity_price/__init__.py
+++ custom_components/electricity_price/__init__.py
@@ -27,9 +27,35 @@ async def async_setup_entry(
     return True
 
 
+async def options_update_listener(
+    hass: core.HomeAssistant, config_entry: config_entries.ConfigEntry
+):
+    """Handle options update."""
+    await hass.config_entries.async_reload(config_entry.entry_id)
+
+
+async def async_unload_entry(
+    hass: core.HomeAssistant, entry: config_entries.ConfigEntry
+) -> bool:
+    """Unload a config entry."""
+    unload_ok = all(
+        await asyncio.gather(
+            *[hass.config_entries.async_forward_entry_unload(entry, "sensor")]
+        )
+    )
+    # Remove options_update_listener.
+    hass.data[DOMAIN][entry.entry_id]["unsub_options_update_listener"]()
+
+    # Remove config entry from domain.
+    if unload_ok:
+        hass.data[DOMAIN].pop(entry.entry_id)
+
+    return unload_ok
+
+
 async def async_setup(hass: core.HomeAssistant, config: dict) -> bool:
     """Set up the Electricity Price component."""
     # @TODO: Add setup code.
-    hass.states.async_set("hello_state.world", "Paulus")
-
+    # hass.states.async_set("hello_state.world", "Paulus")
+    hass.data.setdefault(DOMAIN, {})
     return True
